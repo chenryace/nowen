e54ae2985d40c3660f6a641db49125c73921e150
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-18 06:31:18 +0800

    Add files via upload

---
+++ components/editor/save-button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import useI18n from 'libs/web/hooks/use-i18n';
+import { SaveOutlined } from '@material-ui/icons';
+
+interface SaveButtonProps {
+    onSave: () => void;
+    disabled?: boolean;
+}
+
+const SaveButton: React.FC<SaveButtonProps> = ({ onSave, disabled }) => {
+    const { t } = useI18n();
+    
+    return (
+        <Button 
+            variant="contained" 
+            color="primary" 
+            startIcon={<SaveOutlined />}
+            onClick={onSave}
+            disabled={disabled}
+            size="small"
+            className="ml-2"
+        >
+            {t('Save')}
+        </Button>
+    );
+};
+
+export default SaveButton;
