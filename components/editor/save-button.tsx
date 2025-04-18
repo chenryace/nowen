8a663bf276cf8ded0d64084af1bb4d9ef199f9e6
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-18 16:45:54 +0800

    Update save-button.tsx

--- components/editor/save-button.tsx
+++ components/editor/save-button.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import useI18n from 'libs/web/hooks/use-i18n';
-import { SaveOutlined } from '@material-ui/icons';
+// 移除 Material-UI 图标导入
+// import { SaveOutlined } from '@material-ui/icons';
 
 interface SaveButtonProps {
     onSave: () => void;
@@ -15,7 +16,44 @@ const SaveButton: React.FC<SaveButtonProps> = ({ onSave, disabled }) => {
         <Button
             variant="contained"
             color="primary"
-            startIcon={<SaveOutlined />}
+            // 使用自定义 CSS 图标替代 Material-UI 图标
+            startIcon={
+                <div style={{
+                    width: '16px',
+                    height: '16px',
+                    position: 'relative',
+                    display: 'inline-block',
+                    marginRight: '4px'
+                }}>
+                    <div style={{
+                        width: '12px',
+                        height: '14px',
+                        border: '1px solid currentColor',
+                        borderRadius: '1px',
+                        position: 'absolute',
+                        top: '0',
+                        left: '2px'
+                    }}></div>
+                    <div style={{
+                        width: '8px',
+                        height: '2px',
+                        backgroundColor: 'currentColor',
+                        position: 'absolute',
+                        top: '8px',
+                        left: '4px'
+                    }}></div>
+                    <div style={{
+                        width: '0',
+                        height: '0',
+                        borderStyle: 'solid',
+                        borderWidth: '0 0 4px 4px',
+                        borderColor: 'transparent transparent currentColor transparent',
+                        position: 'absolute',
+                        top: '0',
+                        right: '2px'
+                    }}></div>
+                </div>
+            }
             onClick={onSave}
             disabled={disabled}
             size="small"
